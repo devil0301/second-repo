6ecf0505e5148a6e30bc5a8e691cbd504dc1130e
devil0301 <127320361+devil0301@users.noreply.github.com>
2023-03-08 20:36:53 +0530

    Create mainfile.java

---
+++ mainfile.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n,temp=0,count=0;
+    
+        System.out.println("Enter number:");
+        n=sc.nextInt();
+        int sr=n;
+        //System.out.println(sr);
+        for(int i=1;i<n;i++){
+            if (i%2!=0)   {
+                temp=i;
+            
+                sr=sr-temp;
+                count++;
+                if(sr==0){
+                System.out.println(count);
+                }
+            }
+        }
+        
+        
+    }
+}
